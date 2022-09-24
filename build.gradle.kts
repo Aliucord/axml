060040e421ad18705d391a15044684f65ffe2557
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2022-09-23 21:57:26 -0700

    feat: maven publishing & workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
-group = "com.aliucord.axml"
+group = "com.aliucord"
 version = "1.0.0"
 
 plugins {
     id("java-library")
+    id("maven-publish")
 }
 
 repositories {
@@ -11,4 +12,35 @@ repositories {
 
 dependencies {
     implementation("org.slf4j:slf4j-api:2.0.1")
+}
+
+java {
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        register("axml", MavenPublication::class) {
+            artifactId = "axml"
+            artifact(tasks["jar"])
+            artifact(tasks["sourcesJar"])
+        }
+    }
+
+    repositories {
+        val username = System.getenv("MAVEN_USERNAME")
+        val password = System.getenv("MAVEN_PASSWORD")
+
+        if (username != null && password != null) {
+            maven {
+                credentials {
+                    this.username = username
+                    this.password = password
+                }
+                setUrl("https://maven.aliucord.com/snapshots")
+            }
+        } else {
+            mavenLocal()
+        }
+    }
 }
